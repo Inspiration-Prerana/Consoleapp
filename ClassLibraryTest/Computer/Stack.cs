eec06faf734915312d60b1080c9515e4d039f3d8
Prerana Bajracharya <bajracharya2prerana@gmail.com>
2017-01-09 12:03:11 +0545

    .. (#1)
    * New stack added.
    
    * Stack pop method updated. User defined array size.
    
    * Dynamically size of array increased.

---
+++ ClassLibraryTest/Computer/Stack.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTest.Computer
+{
+    public class Stack
+    {
+        private int[] _container;
+        private int count = 0;
+
+
+        public Stack(int Size)
+        {
+            _container = new int[Size];
+        }
+        public int Count { get { return count; } }
+
+        public void Push(int a)
+        {
+            if (count<_container.Length)
+            {
+                _container[count] = a;
+                count++;
+            }
+
+        }
+
+        public int Pop()
+        {
+            int ret = 0;
+            if (count>=0)
+            {
+                ret = _container[count-1];
+                _container[count-1] = 0;
+                count--;
+            }
+
+            return ret;
+        }
+
+
+        public string DisplayAll()
+        {
+            int[] dest = new int[count];
+            Array.Copy(_container, dest, count);
+            string str = string.Join(",", dest);
+            return str;
+        }
+    }
+
+    public class CustomStack
+    {
+        private int[] _container=new int[0];
+
+        public int Count { get { return _container.Length; } }
+        public void Push(int a)
+        {
+            Array.Resize(ref _container, _container.Length + 1);
+            _container[_container.Length - 1] = a;
+        }
+
+        public int Pop()
+        {
+            int ret=0;
+            if (_container.Length>=0)
+            {
+                ret = _container[_container.Length - 1];
+                Array.Resize(ref _container, _container.Length - 1);
+            }
+
+            return ret;
+        }
+
+        public string DisplayAll()
+        {
+            string str = string.Join(",", _container);
+            return str;
+        }
+    }
+}
--- Consoleapp/Program.cs
+++ Consoleapp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using c1=ClassLibraryTest.Computer;
 
 namespace Consoleapp
 {
@@ -16,14 +17,31 @@ namespace Consoleapp
             //r1.Length = 12;
             //Console.WriteLine($"Area : {r1.Area}\nPerimeter : {r1.Perimeter}");
 
-            ClassLibraryTest.math.Rectangle1 r1 = new ClassLibraryTest.math.Rectangle1();
-            Console.WriteLine($"Area : {r1.Area}\nPerimeter : {r1.Perimeter}");
+            //ClassLibraryTest.math.Rectangle1 r1 = new ClassLibraryTest.math.Rectangle1();
+            //Console.WriteLine($"Area : {r1.Area}\nPerimeter : {r1.Perimeter}");
 
-            r1.Length = 10;
-            Console.WriteLine($"Area : {r1.Area}\nPerimeter : {r1.Perimeter}");
+            //r1.Length = 10;
+            //Console.WriteLine($"Area : {r1.Area}\nPerimeter : {r1.Perimeter}");
 
-            r1.Breadth = 12;
-            Console.WriteLine($"Area : {r1.Area}\nPerimeter : {r1.Perimeter}");
+            //r1.Breadth = 12;
+            //Console.WriteLine($"Area : {r1.Area}\nPerimeter : {r1.Perimeter}");
+
+            c1.CustomStack stack = new c1.CustomStack();
+
+            Console.WriteLine($"Number of items : {stack.Count}, TotalItems : {stack.DisplayAll()}");
+
+            stack.Push(1);
+            Console.WriteLine($"Number of items : {stack.Count}, TotalItems : {stack.DisplayAll()}");
+            stack.Push(10);
+            Console.WriteLine($"Number of items : {stack.Count}, TotalItems : {stack.DisplayAll()}");
+            stack.Push(14);
+            stack.Push(3);
+            stack.Push(2);
+            stack.Push(1);
+            Console.WriteLine($"Number of items : {stack.Count}, TotalItems : {stack.DisplayAll()}");
+            stack.Pop();
+            Console.WriteLine($"Number of items : {stack.Count}, TotalItems : {stack.DisplayAll()}");
+
 
             Console.ReadLine();
         }
